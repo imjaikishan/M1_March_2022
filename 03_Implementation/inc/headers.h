739e5b52d3d0a545ac8c72a24e243cda50ab8883
Jai Kishan <43671275+imjaikishan@users.noreply.github.com>
2022-04-02 11:12:50 +0530

    Create headers.h

---
+++ 03_Implementation/inc/headers.h
@@ -0,0 +1,18 @@
+/**
+ * @file headers.h
+ * @author Jai Kishan
+ * @brief All the header files are here 
+ * @version 0.1
+ * @date 2022-04-02
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+#ifndef HEADERS_H
+#define HEADERS_H
+#include<stdio.h>
+#include <stdlib.h>
+#include<string.h>
+#include<ctype.h>
+#include<time.h>
+#endif
